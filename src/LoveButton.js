775967526f29d92e391ad8c45a2c992ef2f029e0
Alex Katopodis <akatopo@users.noreply.github.com>
2017-02-01 18:20:39 +0200

    :lipstick: love number formatting

--- src/LoveButton.js
+++ src/LoveButton.js
@@ -11,10 +11,22 @@ export default function LoveButton({
 
   return (
     <button className={`${baseButtonClasses} ${(isLoved ? 'is-toggled' : '')}`} onClick={onClickWithId}>
-      <div className="bp-btn-label">{isLoved ? totalLoves + 1 : totalLoves}</div>
+      <div className="bp-btn-label">
+        {lengthFilter(String(isLoved ? totalLoves + 1 : totalLoves))}
+      </div>
       <svg className="bp-svg love-icon">
         <use xlinkHref="#love-icon" />
       </svg>
     </button>
   );
+
+  function lengthFilter(s) {
+    const loveDispatcher = new Array(6);
+
+    loveDispatcher[4] =
+    loveDispatcher[5] = (_s) => `${Math.round(+_s / 100) / 10}K`;
+    loveDispatcher[6] = (_s) => `${_s.substr(0, 3)}K`;
+
+    return (loveDispatcher[s.length] || ((x) => x))(s);
+  }
 }
